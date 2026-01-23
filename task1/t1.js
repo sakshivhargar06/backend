b8aff94b6cbba6a64447b45da697667dc854e5cf
sakshi vhargar <sakshivhargar4@gmail.com>
2026-01-23 23:50:13 +0530

    first commit

---
+++ task1/t1.js
@@ -0,0 +1,132 @@
+let s="VHARGAR SAKSHI DHANANJAY";
+console.log(s);
+/*-----------------------------------------*/
+const PI=3.14;
+console.log(PI);
+console.log("\n");
+/*-----------------------------------------*/
+function sum()
+{
+    let a=10,b=15;
+    let c=a+b;
+    return c;
+}
+let result=sum();
+console.log(result);
+console.log("\n");
+/*-----------------------------------------*/
+let i;
+for(i=1;i<10;i++)
+{
+  if(i==5)
+    break;
+console.log(i);
+}
+console.log("\n");
+/******************************************************/
+
+console.log(age);
+var age=20;
+console.log(age);
+console.log("\n");
+/*-----------------------------------------*/
+let city="Pune";
+console.log(city);
+city="Mumbai";
+console.log(city);
+console.log("\n");
+/*-----------------------------------------*/
+const country="India";
+console.log(country);
+//country="America";
+//console.log(country);
+console.log("\n");
+/*-----------------------------------------*/
+const user=
+{
+    name:"abc"
+};
+console.log(user);
+user.name="xyz"
+console.log(user);
+console.log("\n");
+/******************************************************/
+let str="hellow world";
+let n=12345;
+let b=true;
+let arr=[1,"abc",["100"]];
+let obj={};
+console.log(typeof str);
+console.log(typeof n);
+console.log(typeof b);
+console.log(typeof arr);
+console.log(typeof obj);
+console.log("\n");
+/******************************************************/
+
+   let p=10;
+   let q=5;
+console.log(p+q);
+console.log(p-q);
+console.log(p*q);
+console.log(p/q);
+console.log(p%q);
+console.log("\n");
+if(10=="10")
+{
+    console.log("values are same");
+}
+else{
+    console.log("values are different");
+}
+if(10==="10")
+{
+    console.log("value and datatype are same");
+}
+else{
+    console.log("value are same but datatype are different");
+}
+console.log("\n");
+/******************************************************/
+let x=parseInt("10");
+//let c=x+5;
+console.log(x+5);
+console.log("\n");
+let y=(100).toString();
+console.log(y);
+console.log(typeof y);
+console.log("\n");
+let str1="true";
+let z=(str1==="true");
+console.log(z);
+console.log("\n");
+/******************************************************/
+age=18;
+if(age>=18){
+    console.log("Adult");
+}
+else
+{
+    console.log("Child");
+}
+console.log("\n");
+/*-----------------------------------------*/
+no=5;
+if(no%2==0){
+    console.log("Even");
+}
+else
+{
+    console.log("Odd");
+}
+console.log("\n");
+/*-----------------------------------------*/
+marks=98;
+if(marks>=35){
+    console.log("Pass");
+}
+else
+{
+    console.log("Fail");
+}
+/******************************************************/
